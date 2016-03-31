da4255d8b87812225ece90bbf0145b09d3921480
Adam Leung <centribo@hotmail.com>
2016-03-30 23:51:56 -0400

    GLEW setup

--- LearningOpenGL/LearningOpenGL.cpp
+++ LearningOpenGL/LearningOpenGL.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
 // GLEW
 #define GLEW_STATIC
@@ -10,23 +11,33 @@
 #include <GLFW\glfw3.h>
 
 int main(){
+	//Setup & initializations:
+	int width = 800, height = 600;
+
 	glfwInit(); //Initialize GLFW
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //Tell GLFW we're using OpenGL version 3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //Tell GLFW we're going to be using the Core Profile for OpenGL
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //Tell GLFW we're going to be using the core profile for OpenGL
 	//(This will allow GLFW to throw errors if we accidentally use legacy functions. 
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); //Don't let the user resize the window
 
 	//GLFW initialization
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr); //Create window object
+	GLFWwindow* window = glfwCreateWindow(width, height, "LearnOpenGL", nullptr, nullptr); //Create window object
 	if(window == nullptr){
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window); //Make the context of the window the context on the current thread
+	glViewport(0, 0, width, height); //Set the viewport for rendering. (Left, bottom, width, height)
 
-	//GLEW
+	//GLEW initialization
+	glewExperimental = GL_TRUE; //Allow us to use core profile for OpenGL
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
 
 
 	return 0;
